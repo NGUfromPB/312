9b20c0b87ab5d129e5ea2e7ea7380f33ec0f2649
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-04-04 16:05:59 +0300

    Add files via upload

---
+++ src/main/java/nur/Controller/AdminController.java
@@ -0,0 +1,72 @@
+package nur.Controller;
+
+import nur.models.User;
+import nur.service.RoleServ;
+import nur.service.UserServ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@Controller
+public class AdminController {
+    private final UserServ userServ;
+    private final RoleServ roleServ;
+
+    @Autowired()
+    public AdminController(UserServ userServ, RoleServ roleServ) {
+        this.roleServ=roleServ;
+        this.userServ = userServ;
+    }
+
+    @GetMapping("/admin")
+    public String users(Model model) {
+        model.addAttribute("users", userServ.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping("admin/{id}")
+    public String getUser (@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userServ.findById(id));
+        return "user";
+    }
+
+    @GetMapping("/admin/new")
+    public String addUser(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("listRoles",roleServ.getAllRoles());
+        return "new";
+    }
+
+    @PostMapping("/admin/new")
+    public String add(@RequestParam("role") ArrayList<Long> roles,
+                      @ModelAttribute("user") User user) {
+        user.setRoles(roleServ.getRolesById(roles));
+        userServ.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/admin/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userServ.removeUser(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("admin/edit/{id}")
+    public String updateUser(@PathVariable("id") long id, Model model) {
+        model.addAttribute(userServ.findById(id));
+        model.addAttribute("listRoles",roleServ.getAllRoles());
+        return "edit";
+    }
+
+    @PatchMapping("/admin/edit/{id}")
+    public String update(@RequestParam("role")ArrayList<Long> roles,
+                         User user,
+                         @PathVariable("id") long id) {
+        user.setRoles(roleServ.getRolesById(roles));
+        user.setUserId(id);
+        userServ.addUser(user);
+        return "redirect:/admin";
+    }
+}
